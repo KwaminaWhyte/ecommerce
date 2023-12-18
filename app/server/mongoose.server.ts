d8c9a7c2a6b058ea459c3ab782d6d8068c02678c
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2023-12-18 14:43:19 +0000

    A

--- app/server/mongoose.server.ts
+++ app/server/mongoose.server.ts
@@ -7,7 +7,7 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Database connection error:"));
 db.once("open", () => {
-  console.log("Connected to Database");
+  console.log("Connected to Database, App is ready for use.");
 });
 
 export default mongoose;
