f6efe4b385736012f63e657f73c8761c0105512e
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2023-12-18 12:02:57 +0000

    a

--- app.py
+++ app.py
@@ -23,29 +23,18 @@
 # if __name__ == "__main__":
 #     run_react_app()
 
-
-#!/usr/bin/env python3
 import subprocess
 import os
 
+# Set the path to the Node.js app directory
+project_directory = os.path.join(
+    os.path.expanduser('~'), 'Desktop', 'ecommerce')
 
-def run_react_app():
-    try:
-        # Change the path to your React app directory
-        react_app_path = r'\\'
+# Change the current working directory to the Node.js app directory
+os.chdir(project_directory)
 
-        # Navigate to the React app directory
-        os.chdir(react_app_path)
+# Command to install dependencies (if needed)
+subprocess.run(['npm', 'install'], check=True)
 
-        # Install dependencies (optional, if not already installed)
-        # subprocess.run(['npm', 'install'], check=True)
-
-        # Run the React app
-        subprocess.run(['npm', 'run', 'dev'], check=True)
-
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-
-
-if __name__ == "__main__":
-    run_react_app()
+# Command to start the Node.js app
+subprocess.run(['npm', 'start'], check=True)
