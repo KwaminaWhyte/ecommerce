921de6e7e4c556cc712b8df4ef60c0b1185ec378
KwaminaWhyte <stanleykwaminaotabil@gmail.com>
2023-12-18 11:07:38 +0000

    a

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
+# #!/usr/bin/env python3
+# import subprocess
+
+
+# def run_react_app():
+#     try:
+#         # Change the path to your React app directory
+#         react_app_path = '/'
+
+#         # Navigate to the React app directory
+#         subprocess.run(['cd', react_app_path], check=True)
+
+#         # Install dependencies (optional, if not already installed)
+#         # subprocess.run(['npm', 'install'], check=True)
+
+#         # Run the React app
+#         subprocess.run(['npm', 'run', 'dev'], check=True)
+
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error: {e}")
+
+
+# if __name__ == "__main__":
+#     run_react_app()
+
+
 #!/usr/bin/env python3
 import subprocess
+import os
 
 
 def run_react_app():
     try:
         # Change the path to your React app directory
-        react_app_path = '/'
+        react_app_path = r'\\'
 
         # Navigate to the React app directory
-        subprocess.run(['cd', react_app_path], check=True)
+        os.chdir(react_app_path)
 
         # Install dependencies (optional, if not already installed)
         # subprocess.run(['npm', 'install'], check=True)
